0990d29bf611373d56bc9f13e4299d502276bde3
miko <exfar@samir.co.id>
2023-10-05 14:58:26 +0700

    initialize project

---
+++ app/src/main/java/com/exfarnanda1945/diaryapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.exfarnanda1945.diaryapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.exfarnanda1945.diaryapp.ui.theme.DiaryAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiaryAppTheme {
+
+            }
+        }
+    }
+}
