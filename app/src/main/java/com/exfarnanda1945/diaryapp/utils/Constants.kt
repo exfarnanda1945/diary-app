33d423fe74b073535156f1f557c90afdec451c02
miko <exfar@samir.co.id>
2023-10-06 15:19:11 +0700

    add: constant key

---
+++ app/src/main/java/com/exfarnanda1945/diaryapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.exfarnanda1945.diaryapp.utils
+
+object Constants {
+    const val DB_APP_ID = "application-0-tajuh"
+    const val OAUTH_CLIENT_ID = "69638307562-vnipnkl0bfjcr7sq2h338ik7jmqgt4t7.apps.googleusercontent.com"
+
+    const val WRITE_SCREEN_ARGUMENT_KEY = "diaryId"
+}
