f693befe5125c2017406feb20affdc5741fc4065
exfarnanda1945 <exfarnanda1945@gmail.com>
2024-03-27 06:49:55 +0700

    fix: enable edgeToEdge

--- app/src/main/java/com/exfarnanda1945/diaryapp/MainActivity.kt
+++ app/src/main/java/com/exfarnanda1945/diaryapp/MainActivity.kt
@@ -4,23 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.exfarnanda1945.diaryapp.navigation.NavGraph
 import com.exfarnanda1945.diaryapp.navigation.Screen
 import com.exfarnanda1945.diaryapp.ui.theme.DiaryAppTheme
+import com.exfarnanda1945.diaryapp.utils.Constants.APP_ID
+import io.realm.kotlin.mongodb.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             DiaryAppTheme {
                 val controller = rememberNavController()
                 NavGraph(
-                    startDestination = Screen.Authentication.route,
+                    startDestination = getStartDestination(),
                     navHostController = controller
                 )
             }
         }
+    }
+
+    private fun getStartDestination(): String {
+        val user = App.create(APP_ID).currentUser
+
+        return if (user != null && user.loggedIn) Screen.Home.route else Screen.Authentication.route
     }
 }
